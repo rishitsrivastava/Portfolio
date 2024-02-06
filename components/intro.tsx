eebebc3130fb381a2b63352e971b2f1047fc0248
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-06 22:22:28 +0530

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -99,7 +99,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Rishit_s_Resume (2).pdf"
+          href="/Rishit_s_Resume (8).pdf"
           download
         >
           Download CV{" "}
